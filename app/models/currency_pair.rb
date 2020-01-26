c02a42d09bbae11eb959bb18e20443306f5ee683
Andrey Tykva <tykva.andrey@gmail.com>
2020-01-27 01:14:52 +0200

    Fetch weeks & update views

--- app/models/currency_pair.rb
+++ app/models/currency_pair.rb
@@ -4,4 +4,16 @@ class CurrencyPair < ApplicationRecord
   
   enum target: %i[aud bgn brl cad chf cny czk dkk eur gbp hkd hrk huf idr ils inr
                   jpy krw mxn myr nok nzd php pln ron rub sek sgd thb try usd zar]
+  
+  def fetch_weeks
+    weeks = []
+    date = Date.current
+    
+    duration.times do
+      date = date.prev_week
+      weeks << date
+    end
+    
+    weeks
+  end
 end
